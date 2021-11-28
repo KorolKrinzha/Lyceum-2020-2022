7334a6a02491578daaa76de64f7fecb32874e7c4
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2021-11-28 16:40:51 +0300

    Presentation added

--- R/Politics/analysis_results.R
+++ R/Politics/analysis_results.R
@@ -2,14 +2,64 @@ file <- "table_with_frequencies.csv"
 myvalues <- read.csv2(file, header = TRUE)
 #в csv2 сепаратор это ; 
 
-war_word <- myvalues[which(myvalues$word == "dignity"), 
+politics<-tail(names(myvalues), -1)
+politics<-c("JFK","MLK","Reagan","MLK",
+            "Churchill","Churchill",
+            "Churchill","Hirohito","Hirohito",
+            "Yeltsin","Yeltsin","LKY","LKY",
+            "LKY","Milosevic","Milosevic")
+dignity_word <- myvalues[which(myvalues$word == "dignity"), 
               -which(names(myvalues) == "word"),]
  
-#sub and gsub perform replacement 
-#of the first and all matches respectively
+war_word <- myvalues[which(myvalues$word == "war"), 
+                         -which(names(myvalues) == "word"),]
 
+prosperity_word <- myvalues[which(myvalues$word == "prosperity"), 
+                     -which(names(myvalues) == "word"),]
+					 
+					 
+nation_word <- myvalues[which(myvalues$word == "nation"), 
+                     -which(names(myvalues) == "word"),]
 
+government_word <- myvalues[which(myvalues$word == "government"), 
+                     -which(names(myvalues) == "word"),]
 
+barplot(as.numeric(gsub(";", "",dignity_word)), 
+        main = "Использование слова dignity",
+        ylab = "Частота",
+        col = rainbow(16),
+        names.arg = politics,
+        horiz = FALSE,
+        las = 2)
 
-barplot(as.numeric(gsub(";", "",war_word)))
+barplot(as.numeric(gsub(";", "",war_word)), 
+        main = "Использование слова war",
+        ylab = "Частота",
+        col = rainbow(16),
+        names.arg = politics,
+        horiz = FALSE,
+        las = 2)
 
+barplot(as.numeric(gsub(";", "",prosperity_word)), 
+        main = "Использование слова prosperity",
+        ylab = "Частота",
+        col = rainbow(16),
+        names.arg = politics,
+        horiz = FALSE,
+        las = 2)
+
+barplot(as.numeric(gsub(";", "",nation_word)), 
+        main = "Использование слова nation",
+        ylab = "Частота",
+        col = rainbow(16),
+        names.arg = politics,
+        horiz = FALSE,
+        las = 2)
+
+barplot(as.numeric(gsub(";", "",government_word)), 
+        main = "Использование слова government",
+        ylab = "Частота",
+        col = rainbow(16),
+        names.arg = politics,
+        horiz = FALSE,
+        las = 2)		
